14669041d5a49ba8f87475180fcc523d6414ceb4
Aksara0221 <bsb035446@gmail.com>
2025-11-10 13:46:59 +0900

    10week

--- 7week/expressionTree_Wed.py
+++ 7week/expressionTree_Wed.py
@@ -1,8 +1,8 @@
 # ExpressionTree.py
 from binary_tree_Wed import BTNode
 
-# 1. 모스코드 결정트리
-# ===== 모스 코드 테이블 (A~Z) =====
+# 모스코드 결정트리
+# 1. 모스 코드 테이블 (A~Z) 
 MORSE_TABLE = [
     ('A', '.-'),   ('B', '-...'), ('C', '-.-.'), ('D', '-..'),  ('E', '.'),
     ('F', '..-.'), ('G', '--.'),  ('H', '....'), ('I', '..'),   ('J', '.---'),
@@ -12,51 +12,52 @@ MORSE_TABLE = [
     ('Z', '--..'),
 ]
 
-# 2. encoding : 문자 + {공백} -> 모스 부호
+# 2. encoding : 문자+{공백} -> 모스 부호
 def encode(ch):
-    ch = ch.upper()
-    if ch == ' ':
-        return '/' # 단어 구분
-    elif 'A'<= ch <= 'Z':
-        idx = ord(ch)-ord('A')
-        return MORSE_TABLE[idx][1] # 해당 문자 ch의 모스 부호 반환
-    else :
-        return '?' 
-    
+   ch = ch.upper()
+   if ch == ' ':
+      return '/' # 단어 구분
+   elif 'A'<= ch <= 'Z':
+      idx = ord(ch)- ord('A')
+      return MORSE_TABLE[idx][1] # 해당 문자 ch의 모스 부호 반환
+   else :
+      return '?' 
+   
 # 3. decoding : 부호 -> 문자 변환 : 결정트리 기반
-def deconde(root, code):
-    if code == '/':
-        return ' '
-    node = root
-    for c in code : 
-        if c == '.':
-            node = node.left
-        if c == '-':
-            node = node.right
-        if node is None:
-            return "?"
-        
-        if node and node.data :
-            return node.data
-        else:
-            return '?'
-
+def decode(root, code):
+   if code == '/':
+      return ' '
+   node = root
+   for c in code :
+      if c == '.':
+         node = node.left
+      if c == '-':
+         node = node.right
+      if node is None:
+         return "?"
+      
+   if node and node.data :
+      return node.data
+   else:
+      return '?'
+   
 # 4. 결정트리 생성
 def make_morse_tree():
-    root = BTNode(None, None, None)
-    for ch, code in MORSE_TABLE:
-        cur = root
-        for c in code:
-            if c == '.':
-                if cur.left is None:
-                    cur.left = BTNode(None, None, None)
-                cur = cur.left
-            else:
-                if cur.right is None:
-                    cur.right = BTNode(None, None, None)
-                cur = cur.right
-        cur.data = ch
-    return root
+   root = BTNode(None, None, None) 
+   for ch, code in MORSE_TABLE:
+      cur = root
+      for c in code:
+         if c == '.':
+            if cur.left is None:
+               cur.left = BTNode(None, None, None) 
+            cur = cur.left
+         else:
+            if cur.right is None:
+               cur.right = BTNode(None, None, None) 
+            cur = cur.right
+      cur.data = ch
+   return root
+   
 
 
 #===================================================
